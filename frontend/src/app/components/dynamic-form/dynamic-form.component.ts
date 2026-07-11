dd5877f4ea3d2849593fccf71d27fc2796a225d0
vishnu <vishnusaathyagi@gmail.com>
2026-07-12 00:42:46 +0530

    frontend component and service created

---
+++ frontend/src/app/components/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-form',
+  imports: [],
+  templateUrl: './dynamic-form.component.html',
+  styleUrl: './dynamic-form.component.css'
+})
+export class DynamicFormComponent {
+
+}
