d11d8f4d8e819d3334adcba11587660a8f069f7f
vishnu <vishnusaathyagi@gmail.com>
2026-08-16 17:20:21 +0530

    Update environment with live Render backend URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://development-backend-api-url.onrender.com/api' // Replace with development actual deployed Node URL later
+  apiUrl: 'https://dynamic-forms-api3.onrender.com/api' // Replace with development actual deployed Node URL later
 };
